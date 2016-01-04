140edc25e6b169c803644d6aaf8a6f98487f2e3c
unknown <fpvonly@gmail.com>
2016-01-04 03:14:05 +0200

    further reacted

--- js/ui.js
+++ js/ui.js
@@ -21,14 +21,17 @@ var MenuBar = React.createClass({
 		else
 		{
 			document.getElementById('fixed_wrapper').style.display = "block";
-			this.setState({open:true});
-			mainMenuInstance.setState({open:true});
-			mainMenuInstance.refs.sub_menu.setState({open:false});
+			setTimeout(refresh.bind(this), 20);
+			function refresh() {
+				this.setState({open:true});
+				mainMenuInstance.setState({open:true});
+				mainMenuInstance.refs.sub_menu.setState({open:false});
+			}
 		}
 		
 	},
 	render: function() {
-		return <nav id="navigation"><div><a ref="main_menu_btn" className={ (this.state.open) ? 'active' : '' } href="javascript:void(0);" onClick={this.handleMenuBtnClick}>Menu</a></div></nav>;
+		return <nav id="navigation"><div><a ref="main_menu_btn" className={ (this.state.open) ? 'active' : '' } href="javascript:void(0);" onClick={this.handleMenuBtnClick}>&#9776; Menu</a></div></nav>;
 	}
 });
 
@@ -50,15 +53,51 @@ var MainMenu = React.createClass({
 		this.setState({open:false});
 		this.refs.sub_menu.setState({open:false});
 	},
-	showSubMenu: function() {
-		this.refs.sub_menu.setState({open:true});
+	showSubMenu: function( title ) {
+		switch( title ) {
+			case 'Info':
+				var c = this.getInfoPanelContent();
+			break;
+
+			case 'Leaderboard':
+				var c = this.getLeaderboardPanelContent();
+			break;
+
+			case 'Settings':
+				var c = this.getSettingsPanelContent();
+			break;
+
+			case 'Multiplayer Game':
+				var c = this.getMultiplayerPanelContent();
+			break;
+		}	
+
+		this.refs.sub_menu.setState({open:true, menuTitle:title, content:c});		
+	},
+	getInfoPanelContent: function() {
+		var c = 'This is a simple ping pong game created using HTML5\'s Canvas element\n and plain Javascript. ';
+		c += 'The main UI is created using React. ';
+		c += 'Behind all this is running a Node.js server using Express framework. ';
+		c += 'All animations are CSS3 animations exluding the game animations which are made in plain Javascript.\n\n';
+		c += 'In future updates the following features are to be added: settings panel (localstorage saving), scoreboard (database saving) and multiplayer mode. ';
+		return c;
+	},
+	getLeaderboardPanelContent: function() {
+		return 'Under development';
+	},
+	getMultiplayerPanelContent: function() {
+		return 'Under development';
+	},
+	getSettingsPanelContent: function() {
+		return 'Under development';
 	},
 	render: function() {
 		return <div id="main_menu" className={ (this.state.open) ? 'active' : '' }>
 			<MainMenuBtn clickObj={this.initGame} text={"New Game (solo)"} />
-			<MainMenuBtn clickObj={this.showSubMenu} text="Multiplayer Game" />
-			<MainMenuBtn clickObj={this.showSubMenu} text="Settings" />
-			<MainMenuBtn clickObj={this.showSubMenu} text="Info" />
+			<MainMenuBtn clickObj={this.showSubMenu.bind(this, 'Multiplayer Game')} text="Multiplayer Game" />
+			<MainMenuBtn clickObj={this.showSubMenu.bind(this, 'Leaderboard')} text="Leaderboard" />
+			<MainMenuBtn clickObj={this.showSubMenu.bind(this, 'Settings')} text="Settings" />
+			<MainMenuBtn clickObj={this.showSubMenu.bind(this, 'Info')} text="Info" />
 			<MainMenuBtn clickObj={this.closeMenu} text="Close" />
 			
 			<SubMenu ref="sub_menu" />
@@ -78,15 +117,20 @@ var MainMenuBtn = React.createClass({
 
 var SubMenu = React.createClass({
 	getInitialState: function() {
-		return {open:false};
+		return {
+			open:false,
+			menuTitle:'',
+			content:''
+		};
 	},
 	closeSubMenu: function() {
 		this.setState({open:false});
 	},
 	render: function() {
-		return <div id="sub_menu" className={ (this.state.open) ? 'active' : '' } ref="sub_menu_div">
-			<h1>{this.props.text}</h1>
+		return <div id="sub_menu" className={ (this.state.open) ? 'active' : '' } ref="sub_menu_div">			
 			<div className="sub_menu_btn" onClick={this.closeSubMenu}>Close</div>
+			<h1>{this.state.menuTitle}</h1>
+			<div className="sub_menu_content_panel">{this.state.content}</div>
 		</div>
 	}
 });
